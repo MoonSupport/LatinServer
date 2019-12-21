61c3d464ae1c56fa7af6709a8e4e28b1416afc79
ClockMoon <jiwon3346@naver.com>
2019-12-21 12:33:20 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/latinServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/latinServer.dir/main.cpp.o"
+  "latinServer"
+  "latinServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/latinServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
