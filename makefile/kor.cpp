77eb4920c896f0c9124a8f40621b03d42a0c430c
ClockMoon <jiwon3346@naver.com>
2019-12-29 22:55:46 +0900

    reset

--- makefile/kor.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by 문지원 on 21/12/2019.
-//
-#include "main.h"
-
-
-void proc_kor() {
-    printf("Hello, I'm korea()\n");
-}
--- makefile/main.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by 문지원 on 21/12/2019.
-//
-#include <stdio.h>
-
-
-void proc_kor();
-void proc_usa();
-void proc_jpn();
